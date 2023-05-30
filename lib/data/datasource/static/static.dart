ceb4fe9b45b40079aa1b7ae185db4495bda0fec9
ayaz <ayazhasan@web.de>
2023-05-30 11:03:50 +0200

    create startscreen

---
+++ lib/data/datasource/static/static.dart
@@ -0,0 +1,7 @@
+import 'package:forget_me_not/model/constant/imageAsset.dart';
+
+import '../../model/onBoardingModel.dart';
+
+List<OnBoardingModel> onBoardingList = [
+  OnBoardingModel(title: "willcommen", body: "Erstelle deine Check-Liste, wir werden\n dich daran erinnern.",
+      image: ImageAsset.onBordingImageOne)];
---
+++ lib/model/constant/imageAsset.dart
@@ -0,0 +1,6 @@
+
+class ImageAsset {
+
+  static const String rootImages = "assets/images";
+  static const String onBordingImageOne = "$rootImages/start_image.png";
+}
